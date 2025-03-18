b72748e0933c538841a3bc293f994918bcbfe47c
yyshino <1471386835@qq.com>
2025-03-19 00:52:03 +0800

    feat: pausemenu finish

---
+++ pausemenu-template/pausemenu/assets/pause_menu.gd
@@ -0,0 +1,46 @@
+extends CanvasLayer
+
+
+@export var pause_panel: Panel
+@export var bgm_player: AudioStreamPlayer
+@export var pause_sound: AudioStreamPlayer
+
+var pause_time: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var volume_linear = db_to_linear(bgm_player.volume_db)
+	var target_volume = 0.0 if get_tree().paused else 1.0
+	volume_linear = lerp(volume_linear, target_volume, delta * 15.0)
+#	# 转化为计算好的分贝值
+	bgm_player.volume_db = linear_to_db(volume_linear)
+	
+	bgm_player.stream_paused = get_tree().paused and Time.get_ticks_msec() > pause_time + 300
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.keycode == KEY_ESCAPE and event.pressed:
+			if not get_tree().paused:
+				pause()
+			else:
+				unpause()
+
+func pause():
+	get_tree().paused = true
+	pause_panel.visible = true
+	pause_time = Time.get_ticks_msec()
+	pause_sound.play()
+	
+func unpause():
+	get_tree().paused = false
+	pause_panel.visible = false
+	pause_sound.play()
+	
+func quit_game():
+	get_tree().quit()
